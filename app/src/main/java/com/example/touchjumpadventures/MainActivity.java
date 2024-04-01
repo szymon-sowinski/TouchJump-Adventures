b520001c22c5ef05cfee778759ac013f3889053c
Szymon Sowiński <134802662+szymon-sowinski@users.noreply.github.com>
2024-04-01 21:56:59 +0200

    Added button click listeners and GameActivity, set landscape orientation
    Added button click listeners in MainActivity to handle clicks on help, play, and settings buttons. Also added GameActivity.java file for future game functionality. Additionally, set landscape orientation as default in AndroidManifest.xml for MainActivity.

--- app/src/main/java/com/example/touchjumpadventures/MainActivity.java
+++ app/src/main/java/com/example/touchjumpadventures/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.touchjumpadventures;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton helpButton;
+    private ImageButton playButton;
+    private ImageButton settingsButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helpButton = findViewById(R.id.Help);
+        playButton = findViewById(R.id.Start);
+        settingsButton = findViewById(R.id.Settings);
+    }
+    public void StartGame(View view){
+        helpButton.setOnClickListener(v -> {
+            Log.d("HelpButton", "clicked");
+        });
+
+        playButton.setOnClickListener(v -> {
+            Log.d("PlayButton", "clicked");
+        });
+
+        settingsButton.setOnClickListener(v -> {
+            Log.d("SettingsButton", "clicked");
+        });
     }
 }
