8df085324ef31872e1055051cd966c4ad9162d41
Szymon Sowiński <134802662+szymon-sowinski@users.noreply.github.com>
2024-04-04 19:31:18 +0200

    Added BackgroundImage object and update/draw method in GameEngine
    The code diff shows the addition of a BackgroundImage object and a new method, updateAndDrawBackgroundImage(), in the GameEngine class. The BackgroundImage object is initialized in the GameEngine constructor. The updateAndDrawBackgroundImage() method updates the x-coordinate of the BackgroundImage object and draws it on the canvas. If the x-coordinate goes beyond the screen width, it is reset to 0.

--- app/src/main/java/com/example/touchjumpadventures/GameEngine.java
+++ app/src/main/java/com/example/touchjumpadventures/GameEngine.java
@@ -1,7 +1,17 @@
 package com.example.touchjumpadventures;
 
-public class GameEngine {
-    public GameEngine() {
+import android.graphics.Canvas;
 
+public class GameEngine {
+    BackgroundImage backgroundImage;
+    public GameEngine() {
+        backgroundImage = new BackgroundImage();
+    }
+    public void updateAndDrawBackgroundImage(Canvas canvas) {
+        backgroundImage.setX(backgroundImage.getX() - backgroundImage.getVelocity());
+        if(backgroundImage.getX() < -AppConstans.getBitmapBank().getBackgroundWidth()){
+            backgroundImage.setX(0);
+        }
+        canvas.drawBitmap(AppConstans.getBitmapBank().getBackground(), backgroundImage.getX(), backgroundImage.getY(), null);
     }
 }
