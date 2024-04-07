e9726294433b73c037d202227d5421d0142c2a33
Kamil Banaszek <banaszekkamil120@gmail.com>
2024-04-07 18:54:00 +0200

    Update StoneObstacle class with stone movement and random position generation
    The changes include:
    - Adding a new `update()` method to move the stone obstacle based on its velocity
    - Implementing stone wrapping behavior when it goes off-screen
    - Adding a new `generateRandomYPosition()` method to generate a random position for the stone obstacle when it wraps around the screen

--- app/src/main/java/com/example/touchjumpadventures/StoneObstacle.java
+++ app/src/main/java/com/example/touchjumpadventures/StoneObstacle.java
@@ -1,10 +1,12 @@
 package com.example.touchjumpadventures;
 
+import java.util.Random;
+
 public class StoneObstacle {
     private int stoneX, stoneY, currentFrame, velocity;
     public static int maxFrame;
 
-    public StoneObstacle(){
+    public StoneObstacle() {
         stoneX = 150;
         stoneY = AppConstants.SCREEN_HEIGHT / 2 - AppConstants.getBitmapBank().getSquareHeight() / 4 + 250;
         currentFrame = 0;
@@ -16,7 +18,7 @@ public class StoneObstacle {
         return velocity;
     }
 
-    public void setVelocity(int velocity){
+    public void setVelocity(int velocity) {
         this.velocity = velocity;
     }
 
@@ -24,23 +26,47 @@ public class StoneObstacle {
         return currentFrame;
     }
 
-    public void setCurrentFrame(int currentFrame){
+    public void setCurrentFrame(int currentFrame) {
         this.currentFrame = currentFrame;
     }
 
-    public int getX(){
+    public int getX() {
         return stoneX;
     }
 
-    public int getY(){
+    public int getY() {
         return stoneY;
     }
 
-    public void setX(int squareX) {
+    public void setX(int stoneX) {
         this.stoneX = stoneX;
     }
 
-    public void setY(int squareY) {
+    public void setY(int stoneY) {
         this.stoneY = stoneY;
+    }
+
+    // Metoda do aktualizacji pozycji przeszkody na ekranie
+    public void update() {
+        // Aktualizuj pozycję przeszkody na podstawie prędkości
+        stoneX += velocity;
+
+        // Jeśli przeszkoda wychodzi poza lewą krawędź ekranu, przenieś ją na prawą stronę z nową losową pozycją Y
+        if (stoneX < -AppConstants.getBitmapBank().getStoneWidth()) {
+            stoneX = AppConstants.SCREEN_WIDTH; // Przenieś na prawą stronę ekranu
+            stoneY = generateRandomYPosition(); // Ustaw nową losową pozycję Y
+        }
+
+        // Jeśli przeszkoda wychodzi poza prawą krawędź ekranu, przenieś ją na lewą stronę z nową losową pozycją Y
+        if (stoneX > AppConstants.SCREEN_WIDTH) {
+            stoneX = -AppConstants.getBitmapBank().getStoneWidth(); // Przenieś na lewą stronę ekranu
+            stoneY = generateRandomYPosition(); // Ustaw nową losową pozycję Y
+        }
+    }
+
+    // Metoda do generowania losowej pozycji Y dla przeszkody
+    private int generateRandomYPosition() {
+        // Generuj losową pozycję Y w zakresie od 0 do maksymalnej wysokości ekranu
+        return new Random().nextInt(AppConstants.SCREEN_HEIGHT - AppConstants.getBitmapBank().getStoneHeight());
     }
 }
